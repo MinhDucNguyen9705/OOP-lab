4d6f6201bf5247167c08c386ee67fcc433b10b65
MinhDucNguyen9705 <nguyenminhduc9705@gmail.com>
2024-10-22 17:39:44 +0700

    Constructor changed

--- Lab2/AimsProject/src/DigitalVideoDisc.java
+++ Lab2/AimsProject/src/DigitalVideoDisc.java
@@ -7,28 +7,32 @@ public class DigitalVideoDisc {
 	private float cost;
 	
 	public DigitalVideoDisc(String title) {
+		super();
 		this.title = title;
 	}
 	
 	public DigitalVideoDisc(String title, String category, float cost) {
-		this.category = category;
+		super();
 		this.title = title;
+		this.category = category;
 		this.cost = cost;
 	}
 	
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
-		this.director = director;
-		this.category = category;
+		super();
 		this.title = title;
+		this.category = category;
+		this.director = director;
 		this.cost = cost;
 	}
 	
-	public DigitalVideoDisc(String title, String category,String director, int length, float cost) {
-		this.director = director;
-		this.category = category;
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
 		this.title = title;
-		this.cost = cost;
+		this.category = category;
+		this.director = director;
 		this.length = length;
+		this.cost = cost;
 	}
 	
 	public String getTitle() {
