ce0887234b8c87ecb76a7d0e3c7a921c3a749943
MinhDucNguyen9705 <nguyenminhduc9705@gmail.com>
2024-11-19 16:23:16 +0700

    Passing parameter added

--- Lab3/AimsProject/src/general/DigitalVideoDisc.java
+++ Lab3/AimsProject/src/general/DigitalVideoDisc.java
@@ -7,14 +7,10 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
-	private static int nbDigitalVideoDisc = 0;
-	private int id;
 	
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
-		nbDigitalVideoDisc += 1;
-		this.id = nbDigitalVideoDisc;
 	}
 	
 	public DigitalVideoDisc(String title, String category, float cost) {
@@ -22,8 +18,6 @@ public class DigitalVideoDisc {
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
-		nbDigitalVideoDisc += 1;
-		this.id = nbDigitalVideoDisc;
 	}
 	
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
@@ -32,8 +26,6 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
-		nbDigitalVideoDisc += 1;
-		this.id = nbDigitalVideoDisc;
 	}
 	
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -43,8 +35,6 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
-		nbDigitalVideoDisc += 1;
-		this.id = nbDigitalVideoDisc;
 	}
 	
 	public String getTitle() {
@@ -66,35 +56,4 @@ public class DigitalVideoDisc {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public String toString() {
-		String str = this.getId() + "." + "DVD" + " - " + 
-				 this.getTitle() + " - " + 
-				 this.getCategory() + " - " + 
-				 this.getDirector() + " - " +
-				 this.getLength() + ": " + 
-				 this.getCost();
-		return str;
-	}
-	
-	public boolean isMatch(String title) {
-		if (this.getTitle() == title) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
-	public boolean isMatch(int id) {
-		if (this.getId() == id) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
 }
