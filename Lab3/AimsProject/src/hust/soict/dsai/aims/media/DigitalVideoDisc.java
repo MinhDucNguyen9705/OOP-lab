64afdbf1d03f40c1e76022804a5c69099e25fc74
MinhDucNguyen9705 <nguyenminhduc9705@gmail.com>
2024-11-26 14:12:59 +0700

    Major changes

--- Lab3/AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ Lab3/AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,74 +1,32 @@
-package hust.soict.dsai.aims.disc;
+package hust.soict.dsai.aims.media;
 
 
-public class DigitalVideoDisc {
-	private String title;
-	private String category;
-	private String director;
-	private int length;
-	private float cost;
+public class DigitalVideoDisc extends Disc{
 	private static int nbDigitalVideoDisc = 0;
 	private int id;
 	
 	public DigitalVideoDisc(String title) {
-		super();
-		this.title = title;
+		super(title);
 		nbDigitalVideoDisc += 1;
 		this.id = nbDigitalVideoDisc;
 	}
 	
 	public DigitalVideoDisc(String title, String category, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
+		super(title, category, cost);
 		nbDigitalVideoDisc += 1;
 		this.id = nbDigitalVideoDisc;
 	}
 	
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.cost = cost;
+		super(title, category, director, cost);
 		nbDigitalVideoDisc += 1;
 		this.id = nbDigitalVideoDisc;
 	}
 	
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.length = length;
-		this.cost = cost;
+		super(title, category, director, length, cost);
 		nbDigitalVideoDisc += 1;
 		this.id = nbDigitalVideoDisc;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public String getDirector() {
-		return director;
-	}
-	public int getLength() {
-		return length;
-	}
-	public float getCost() {
-		return cost;
-	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	public int getId() {
-		return id;
 	}
 	
 	public String toString() {
